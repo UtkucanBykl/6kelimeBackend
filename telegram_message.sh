ed82a7b592e49d1a77981c50076575025a8924ec
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2019-02-26 17:45:54 +0300

    final fix typo

--- telegram_message.sh
+++ telegram_message.sh
@@ -2,7 +2,6 @@
 
 TOKEN=$TOKEN
 CHAT_ID=$CHAT_ID
-COMMIT_URL='https://github.com/UtkucanBykl/6kelimeBackend/commit/'
 MESSAGE='Status :'$STATU 'Commit => '$COMMIT_URL$TRAVIS_COMMIT' Commit Message: '$TRAVIS_COMMIT_MESSAGE' Event Type: '$TRAVIS_EVENT_TYPE' Branch: '$TRAVIS_PULL_REQUEST_BRANCH
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 

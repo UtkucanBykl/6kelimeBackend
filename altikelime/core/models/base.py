844cef8195187b44d31ed2b39c32af2d50a1de6a
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2019-03-01 22:56:15 +0300

    Add queryset .activies() and .inspecting()

--- altikelime/core/models/base.py
+++ altikelime/core/models/base.py
@@ -1,7 +1,20 @@
 from django.db import models
-from ..constants import STATUS, OPEN
+from ..constants import STATUS, OPEN, INSPECTING
 
-__all__ = ['BaseModel']
+__all__ = ['BaseModel', 'BaseModelQuerySet']
+
+
+class BaseModelQuerySet(models.QuerySet):
+    """
+    .actives() return STATUS OPEN
+    .INSPECTING() return STATUS INSPECTING
+    """
+
+    def actives(self):
+        return self.filter(status=OPEN)
+
+    def inspecting(self):
+        return self.filter(status=INSPECTING)
 
 
 class BaseModel(models.Model):
@@ -9,6 +22,8 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     status = models.CharField(choices=STATUS, default=OPEN, max_length=10)
+
+    objects = BaseModelQuerySet.as_manager()
 
     class Meta:
         abstract = True

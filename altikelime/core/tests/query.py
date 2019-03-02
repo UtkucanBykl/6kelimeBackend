a0e702e697411febe838b74d117db13ca8c12287
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2019-03-02 11:15:38 +0300

    Fix typo

--- altikelime/core/tests/query.py
+++ altikelime/core/tests/query.py
@@ -6,6 +6,6 @@ from ..models import Category
 class QueryTestCase(TestCase):
 
     def test_get_post_activies(self):
-        category = Category.objects.create(status='OPEN', name='Cat1')
-        category2 = Category.objects.create(status='CLOSE', name='Cat2')
+        Category.objects.create(status='OPEN', name='Cat1')
+        Category.objects.create(status='CLOSE', name='Cat2')
         self.assertEquals(Category.objects.filter(status='OPEN').count(), Category.objects.actives().count())

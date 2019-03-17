468834e981c7ef14b72bf17e941e5a2949186f9d
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2019-03-17 23:19:57 +0300

    add local storage. we are going to improve storage for token.

--- altikelime/core/views/cateogry_posts.py
+++ altikelime/core/views/cateogry_posts.py
@@ -1,5 +1,5 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
 
 from ..serializers import PostListSerializer
 from ..models import Post
@@ -9,10 +9,11 @@ __all__ = ['CategoryPostsListView']
 
 
 class CategoryPostsListView(ListAPIView):
+    authentication_classes = (TokenAuthentication,)
     serializer_class = PostListSerializer
     queryset = Post.objects.actives().filter(publish=True)
-    permission_classes = (AllowAny, )
 
     def get_queryset(self):
+        print(self.request.user)
         qs = super(CategoryPostsListView, self).get_queryset()
         return qs.filter(category__name=self.kwargs['cat_name'])
